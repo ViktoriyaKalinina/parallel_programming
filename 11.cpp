e3e7c5a576483d64dbf7925a7e2188ed4cb749f8
Bulat Ismagilov <ismglvb@yandex.ru>
2018-03-22 20:20:39 +0300

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,21 @@
+#include <omp.h>
+#include <stdio.h>
+
+#define N 30
+#define D 7
+
+int main () {
+    int a[N];
+    int max = 0;
+
+    #pragma omp parallel for num_threads(3) shared(max)
+    for (int i = 0; i < N; i++) {
+        a[i] = rand() % N;
+        if (a[i] % D == 0 && max < a[i]) {
+            #pragma omp critical
+            max = a[i];
+        }
+        printf(" %d \n", a[i]);
+    }
+    printf("max is %d\n", max);
+}
