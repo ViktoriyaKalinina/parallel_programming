af0b78b70fe6c2bc5e2dc4b0c5dbcfe75a3fd5c2
Bulat Ismagilov <ismglvb@yandex.ru>
2018-03-23 09:54:57 +0300

    updt

--- 9.cpp
+++ 9.cpp
@@ -1,45 +1,43 @@
 #include <omp.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
 
 #define R 30
 
-int main () {
-    
-    int n = 6;
-    int m = 8;
-    
-    int d[n][m];
+int main (int argc, char *argv[]) {
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            d[i][j] = rand() % R;
-            printf("%d ", d[i][j]);
-        }
-        printf("\n");
+  int n = 6;
+  int m = 8;
+
+  int d[n][m];
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      d[i][j] = rand() % R;
+      printf("%d ", d[i][j]);
     }
     printf("\n");
-    printf("\n");
+  }
+  printf("\n");
+  printf("\n");
 
-    int max = 0;
-    int min = 0;
+  int max = 0;
+  int min = 0;
 
-    int count = 0;
-    #pragma omp parallel num_threads(3) reduction (+: count)
-    for (int i = 0; i < n; i++) {
-        #pragma omp for
-		for (int j = 0; j < m; j++) {
-			count++;
+  int count = 0;
+#pragma omp parallel num_threads(3) reduction (+: count)
+  for (int i = 0; i < n; i++) {
+#pragma omp for
+    for (int j = 0; j < m; j++) {
+      count++;
 
-			if (min > d[i][j]) {
-				#pragma omp critical
-				min = d[i][j];
-			}
-			if (max < d[i][j]) {
-				#pragma omp critical
-				max = d[i][j];
-			}
-		}
-	}
-	printf("count: %d, max value is %d, min value is %d\n", count, max, min);  
+  #pragma omp critical
+      if (min > d[i][j])
+        min = d[i][j];
+  #pragma omp critical
+      if (max < d[i][j])
+        max = d[i][j];
+    }
+  }
+  printf("count: %d, max value is %d, min value is %d\n", count, max, min);  
 }
