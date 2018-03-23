f880669c4f0ce29713fc4eb6501c2720332b9f29
Bulat Ismagilov <ismglvb@yandex.ru>
2018-03-23 09:54:07 +0300

    updt

--- 11.cpp
+++ 11.cpp
@@ -8,13 +8,12 @@ int main () {
     int a[N];
     int max = 0;
 
-    #pragma omp parallel for num_threads(3) shared(max)
+#pragma omp parallel for num_threads(3) shared(max)
     for (int i = 0; i < N; i++) {
         a[i] = rand() % N;
-        if (a[i] % D == 0 && max < a[i]) {
-            #pragma omp critical
+        #pragma omp critical
+          if (a[i] % D == 0 && max < a[i])
             max = a[i];
-        }
         printf(" %d \n", a[i]);
     }
     printf("max is %d\n", max);
