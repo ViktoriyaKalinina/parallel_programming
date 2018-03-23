b94c5a8478f944882f5ed9975041cabe977007aa
Bulat Ismagilov <ismglvb@yandex.ru>
2018-03-23 09:57:36 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,15 +1,33 @@
 #include <omp.h>
-#include <iostream>
 #include <stdio.h>
 
-int main (){
-	int this_thread = omp_get_thread_num(), num_threads = omp_get_num_threads();
-	#pragma omp parallel if(num_threads > 1)
-	{	
-		#pragma omp parallel num_threads(3)
-			printf ("Number of threads - %d, Threads number - %d, Hello world!\n", omp_get_num_threads(), omp_get_thread_num());
 
-  	#pragma omp parallel num_threads(1)
-    	printf ("Number of threads - %d, Threads number - %d, Hello world!", omp_get_num_threads(), omp_get_thread_num());
+int main (){
+{
+	omp_set_dynamic(0);
+	omp_set_num_threads(3);
+	int thread_number;
+  int threads;
+#pragma omp parallel private(threads, thread_number)
+	{
+		threads = omp_get_num_threads();
+		thread_number = omp_get_thread_num();
+	#pragma omp parallel if (threads > 1)
+		{
+			printf("Number of Threads - %d\n", thread_number);
+			printf("Thread - %d\n", threads);
+		}
 	}
+	omp_set_num_threads(1);
+#pragma omp parallel private(threads, thread_number)
+	{
+		int threads = omp_get_num_threads();
+		int thread_number = omp_get_thread_num();
+	#pragma omp parallel if (threads > 1)
+		{
+			printf("Number of Threads - %d\n", thread_number);
+			printf("Thread - %d\n", threads);
+		}
+	}
+  }
 }
